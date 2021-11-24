d1224b0871c5a94e3b8f579fc84a5891572548f5
bigerfe <223344386@qq.com>
2021-11-24 14:59:34 +0800

    feat: test

--- src/generator-address/index.js
+++ src/generator-address/index.js
@@ -1,6 +1,6 @@
 // github https://github.com/moonrailgun/chinese-address-generator
 const generator = require('chinese-address-generator/generator4');
-let i=0;
+let i=1;
 while (i<100) {
   const address = generator.fabricateFullAddress(); // => {region: "上海", code: "310000"}
   console.log(address);
